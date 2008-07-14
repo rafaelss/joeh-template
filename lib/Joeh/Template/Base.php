cd30f9a01ff7c3c638b758238c3f4b087595a657
unknown <rafael.ssouza@gmail.com>
2008-07-14 16:50:58 -0300

    added helper support

--- lib/Joeh/Template/Base.php
+++ lib/Joeh/Template/Base.php
@@ -1,4 +1,30 @@
 <?php
+/*
+The MIT License
+
+Copyright (c) 2008 Rafael Souza <rafael@joeh.com.br>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+require_once LIB_PATH . 'Joeh/Template/Helper.php';
+
 abstract class Joeh_Template_Base {
 
     #######################
@@ -16,13 +42,13 @@ abstract class Joeh_Template_Base {
     #####################
     ## PRIVATE PROPERTIES
     #####################
-    
+
     private $variables = array();
 
     ################
     ## MAGIC METHODS
     ################
-    
+
     public function __get($name) {
         return $this->variables[$name];
     }
@@ -52,6 +78,10 @@ abstract class Joeh_Template_Base {
         else {
             $this->{$name} = $value;
         }
+    }
+
+    public function addHelper(Joeh_Template_Helper $helper) {
+        $this->assign($helper->getName(), $helper);
     }
 
     public function render($name, $return = false) {
