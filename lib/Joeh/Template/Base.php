fd530b7b318b0361e6fc90e841a6169188ea2807
unknown <rafael.ssouza@gmail.com>
2008-07-14 12:10:30 -0300

    added $return parameter to render method

--- lib/Joeh/Template/Base.php
+++ lib/Joeh/Template/Base.php
@@ -9,37 +9,42 @@ abstract class Joeh_Template_Base {
 
     protected $extension = 'tpl';
 
-  #################
-  ## PUBLIC METHODS
-  #################
+    #################
+    ## PUBLIC METHODS
+    #################
 
-  public function render($name) {
-    $contents = null;
-    $lines = $this->read($name);
-    foreach($lines as $index => $line) {
-        if(strpos($line, '<?') !== false) {
-            if(strpos($line, '?>') === false || strpos($line, '<?php') !== false) {
-                throw new RuntimeException("Syntax error in line " . ($index+1));
+    public function render($name, $return = false) {
+        $contents = null;
+        $lines = $this->read($name);
+        foreach($lines as $index => $line) {
+            if(strpos($line, '<?') !== false) {
+                if(strpos($line, '?>') === false || strpos($line, '<?php') !== false) {
+                    throw new RuntimeException("Syntax error in line " . ($index+1));
+                }
             }
+
+            $line = preg_replace('/<\?[^=]/', '<?php ', $line);
+            $line = preg_replace('/<\?=/', '<?php echo', $line);
+
+            $contents .= $line;
         }
 
-        $line = preg_replace('/<\?[^=]/', '<?php ', $line);
-        $line = preg_replace('/<\?=/', '<?php echo', $line);
+        $this->save($name, $contents);
 
-        $contents .= $line;
+        ob_start();
+        include($this->compilePath . "{$name}.{$this->extension}.php");
+        $result = ob_get_clean();
+
+        if($return) {
+            return $result;
+        }
+
+        echo $result;
     }
 
-    $this->save($name, $contents);
-
-    ob_start();
-    include($this->compilePath . "{$name}.{$this->extension}.php");
-    $result = ob_get_clean();
-    return $result;
-  }
-
-  ##################
-  ## PRIVATE METHODS
-  ##################
+    ##################
+    ## PRIVATE METHODS
+    ##################
 
     private function read($name) {
         return file($this->basePath . $name . '.' . $this->extension);
